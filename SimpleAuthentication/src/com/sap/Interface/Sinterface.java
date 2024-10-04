a7d43d729b4140aec33fddc66599b95bd1523861
indhumathi64 <indhuyazhini64@gmail.com>
2024-10-04 18:45:43 +0530

    first commit

---
+++ SimpleAuthentication/src/com/sap/Interface/Sinterface.java
@@ -0,0 +1,9 @@
+package com.sap.Interface;
+
+import com.sap.Bean.RegisterBean;
+
+public interface Sinterface {
+
+		public int register(RegisterBean rb);
+		public int login(String username,String password);
+}
